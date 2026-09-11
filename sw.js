00a3841af0e6d72bfd3c16e9f66d8f3986eec91f
zhaozirui666888-creator <zhaozirui666888@gmail.com>
2026-09-12 01:11:43 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='kaoyan-pwa-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./v2.js','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
